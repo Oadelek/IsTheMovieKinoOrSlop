7a4588ba6b6dc4d1211e54740cb0344e0bc74582
Oluwasesan Adeleke <oadeleke@algomau.ca>
2024-07-21 23:21:53 +0000

    Created the main App.php to handle routing

--- app/helper/init.php
+++ app/helper/init.php
@@ -1,21 +1,18 @@
 <?php
-error_reporting(0);
-ini_set('session.gc_maxlifetime', 28800);
-ini_set('session.gc_probability', 1);
-ini_set('session.gc_divisor', 1);
-$sessionCookieExpireTime = 28800; // 8hrs
-session_set_cookie_params($sessionCookieExpireTime);
-session_start();
-
 require_once 'Database.php';
 require_once 'Gemini.php';
 require_once 'OMDB.php';
-require_once '../core/App.php';
-require_once '../core/Config.php';
-require_once '../core/Controller.php';
 
-spl_autoload_register(function($class) {
-    if (file_exists('../models/' . $class . '.php')) {
-        require_once '../models/' . $class . '.php';
-    }
-});
+// Load core libraries
+require_once 'app/core/App.php';
+require_once 'app/core/Config.php';
+require_once 'app/core/Controller.php';
+
+// Load models
+require_once 'app/models/UserModel.php';
+require_once 'app/models/MovieModel.php';
+require_once 'app/models/ReviewModel.php';
+require_once 'app/models/LogModel.php';
+
+// Start session
+session_start();
