91f942d6c96dbcc24193bcbfb54feac78faa9c74
Oluwasesan Adeleke <oadeleke@algomau.ca>
2024-07-21 23:44:39 +0000

    Modified for better error handling

--- app/helper/OMDB.php
+++ app/helper/OMDB.php
@@ -1,10 +1,43 @@
 <?php
 class OMDB {
-    private $apiKey = $_ENV['OMDB_KEY'];
+    private $apiKey;
+    private $baseUrl = 'http://www.omdbapi.com/';
+
+    public function __construct() {
+        $this->apiKey = $_ENV['OMDB_API_KEY'];
+    }
 
     public function getMovieDetails($title) {
-        $url = "http://www.omdbapi.com/?t=" . urlencode($title) . "&apikey=" . $this->apiKey;
-        $response = file_get_contents($url);
-        return json_decode($response, true);
+        $query = [
+            't' => $title,
+            'apikey' => $this->apiKey
+        ];
+        return $this->makeRequest($query);
+    }
+
+    private function makeRequest($query) {
+        $url = $this->baseUrl . '?' . http_build_query($query);
+
+        $options = [
+            'http' => [
+                'method'  => 'GET',
+                'timeout' => 10 // Timeout in seconds
+            ]
+        ];
+
+        $context = stream_context_create($options);
+        $response = @file_get_contents($url, false, $context);
+
+        if ($response === FALSE) {
+            return ['error' => 'Unable to fetch data from OMDB API.'];
+        }
+
+        $data = json_decode($response, true);
+
+        if (isset($data['Error'])) {
+            return ['error' => $data['Error']];
+        }
+
+        return $data;
     }
 }
