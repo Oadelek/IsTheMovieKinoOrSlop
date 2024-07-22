31b5f352b8da3eeb4a4799952d50e428b3d36a68
Oluwasesan Adeleke <oadeleke@algomau.ca>
2024-07-22 02:32:50 +0000

    Added the search functionality to show on view

--- app/views/movies/SearchView.php
+++ app/views/movies/SearchView.php
@@ -4,15 +4,22 @@
         <input type="text" name="query" placeholder="Search for a movie...">
         <button type="submit">Search</button>
     </form>
-    <?php if (isset($data['movies'])): ?>
+    <?php if (isset($data['movies']) && !empty($data['movies'])): ?>
         <ul>
             <?php foreach ($data['movies'] as $movie): ?>
                 <li>
                     <a href="/movie/details/<?php echo $movie['id']; ?>">
-                        <?php echo $movie['title']; ?> (<?php echo $movie['year']; ?>)
+                        <?php echo htmlspecialchars($movie['title']); ?> (<?php echo htmlspecialchars($movie['year']); ?>)
                     </a>
+                    <?php if (isset($movie['omdb_data'])): ?>
+                        <p>Director: <?php echo htmlspecialchars($movie['omdb_data']['Director']); ?></p>
+                        <p>Plot: <?php echo htmlspecialchars($movie['omdb_data']['Plot']); ?></p>
+                        <img src="<?php echo htmlspecialchars($movie['omdb_data']['Poster']); ?>" alt="Poster">
+                    <?php endif; ?>
                 </li>
             <?php endforeach; ?>
         </ul>
+    <?php elseif (isset($data['movies'])): ?>
+        <p>No movies found.</p>
     <?php endif; ?>
 </main>
