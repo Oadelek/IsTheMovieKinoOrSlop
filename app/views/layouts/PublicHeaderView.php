deb6eefa39e7be43e3dcce6d1d7d5252b226c052
Oluwasesan Adeleke <oadeleke@algomau.ca>
2024-07-22 00:12:47 +0000

    header view for non-users

---
+++ app/views/layouts/PublicHeaderView.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Is The Movie Kino Or Sleep</title>
+    <link rel="stylesheet" href="/public/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/movies/search">Search Movies</a></li>
+                <li><a href="/auth/login">Login</a></li>
+                <li><a href="/auth/register">Register</a></li>
+            </ul>
+        </nav>
+    </header>
