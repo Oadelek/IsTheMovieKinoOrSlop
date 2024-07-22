bd791a966acf2bca62601e8767d0a64df1d5d299
Oluwasesan Adeleke <oadeleke@algomau.ca>
2024-07-22 00:12:01 +0000

    My first html file for Kino Kino :)

---
+++ app/views/layouts/PrivateHeaderView.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Is The Movie Kino Or Sleep</title>
+    <link rel="stylesheet" href="/public/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/movies/search">Search Movies</a></li>
+                <li><a href="/auth/logout">Logout</a></li>
+            </ul>
+        </nav>
+    </header>
