c01d69bc7432a91adcc77e22094b34ad24cf2d31
Oluwasesan Adeleke <oadeleke@algomau.ca>
2024-07-22 00:01:00 +0000

    Modified the log model

---
+++ app/models/LogModel.php
@@ -0,0 +1,31 @@
+<?php
+class LogModel {
+    private $db;
+
+    public function __construct() {
+        $this->db = new Database();
+        $this->db = $this->db->connect();
+    }
+
+    public function addLog($data) {
+        $sql = "INSERT INTO user_viewing_history (user_id, movie_id) VALUES (:user_id, :movie_id)";
+        $stmt = $this->db->prepare($sql);
+
+        $stmt->bindParam(':user_id', $data['user_id']);
+        $stmt->bindParam(':movie_id', $data['movie_id']);
+
+        if($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getLog($user_id) {
+        $sql = "SELECT * FROM user_viewing_history WHERE user_id = :user_id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(':user_id', $user_id);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
